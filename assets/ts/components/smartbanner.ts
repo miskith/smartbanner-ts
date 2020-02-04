a18d45aa33ccc4f1bc93c28e997e5e8d461a660d
David Myška <miskith@gmail.com>
2020-02-04 13:35:54 +0100

    Main script to be JS

--- assets/ts/components/smartbanner.ts
+++ assets/ts/components/smartbanner.ts
@@ -2,7 +2,7 @@ import crumbs from 'crumbsjs';
 import { smartbannerOptions } from '@interfaces/smartbannerOptions';
 require('../../scss/smartbanner.scss');
 
-export class SmartBanner
+export default class SmartBanner
 {
 	private node: Node = null;
 	private readonly storageKey = 'smartbanner-ts';
